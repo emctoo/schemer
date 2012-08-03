568f39ab2c578488dcd478bc2f508bcafa642866
Maple H.L <Maple.HL@gmail.com>
2012-08-03 21:33:12 +0800

    no test

--- lexer.c
+++ lexer.c
@@ -35,12 +35,29 @@ typedef struct {
 	int len;
 } token_t;
 
-bool token_any(char _c, token_t *token) {
-	char any[] = "[]{}|(}'`.", c[1] = { _c };
+bool token_any(program_t *program, token_t *token, char any[]) {
+	char c[1] = { program->code[program->iter];};
 	int idx = strspn(c, any);
 	if(idx >= strlen(any)) { return false; }
 	token->token[0] = c[0];
 	token->len = 1;
+}
+
+const char token_single[] = "[]{}|(}'`.";
+const char token_prfix[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$%&*/:<=>?^_~";
+const char token_in[] = "1234567890+-.@";
+	
+bool token_identifier(program_t *program, token_t *token) {
+	int token_len = 0;
+	while ( isblank(program->code[program->iter] ) {
+		token->token[token_len] = program->code[program->iter];
+		token->len++;
+		if ( token_len = token->len ) { return false; }
+		
+		program->iter++;
+		program->col++;		
+	}
+	return true;
 }
 
 bool token_next(program_t *program, token_t *token) {
@@ -63,10 +80,12 @@ bool token_next(program_t *program, token_t *token) {
 		token->code[0] = c, token->code[1] = n, token->len = 2;
 		program->iter += 2, program->cos += 2;
 		return true;
-	} 
+	} else if ( token_any(program, token, token_prefix) ) {
+		
+	}
 	
 	
-	if(token_any(code[program->iter], token)) {
+	if(token_any(program, token, token_single)) {
 		program->iter++;
 		program->col++;
 	}  else if ( c == '#' && ( n == '(' || n == 'f' || n == 't')) {		
